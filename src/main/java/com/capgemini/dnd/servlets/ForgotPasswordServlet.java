add2e36cf9f16a657390dc280ba011f94549f2bc
gaikwadgaurav997 <37701250+gaikwadgaurav997@users.noreply.github.com>
2019-10-22 11:07:39 +0530

    doOptions login part

--- src/main/java/com/capgemini/dnd/servlets/ForgotPasswordServlet.java
+++ src/main/java/com/capgemini/dnd/servlets/ForgotPasswordServlet.java
@@ -48,14 +48,7 @@ public class ForgotPasswordServlet extends HttpServlet {
 			throws ServletException, IOException {
 	}
 
-	@Override
-	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json");
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Authorization, Content-Length, X-Requested-With");
-		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
-	}
+	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
