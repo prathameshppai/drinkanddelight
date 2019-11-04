4da8c0552f816338922d411bb01f45ded0e4fa3e
Prathamesh Pai <prathameshppai@gmail.com>
2019-11-04 09:04:06 +0530

    controller added

--- src/main/java/com/capgemini/dnd/service/RawMaterialService.java
+++ src/main/java/com/capgemini/dnd/service/RawMaterialService.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.capgemini.dnd.customexceptions.BackEndException;
 import com.capgemini.dnd.customexceptions.ConnectionException;
 import com.capgemini.dnd.customexceptions.DisplayException;
@@ -24,6 +26,7 @@ import com.capgemini.dnd.dto.RawMaterialOrder;
 import com.capgemini.dnd.dto.RawMaterialStock;
 import com.capgemini.dnd.dto.Supplier;
 
+@Service
 public interface RawMaterialService {
 //	public void viewRMspec(String name);
 
