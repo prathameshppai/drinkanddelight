182ba9d7fbc7c8e2349f46a40d7d6d82d7db0140
Prathamesh Pai <prathameshppai@gmail.com>
2019-11-04 17:58:39 +0530

    getdetails and placeorder (RM and Product) Controller added
    All 4 controllers work

--- src/main/java/com/capgemini/dnd/servlets/PlaceRMOrderServlet.java
+++ src/main/java/com/capgemini/dnd/servlets/PlaceRMOrderServlet.java
@@ -6,12 +6,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.capgemini.dnd.customexceptions.ConnectionException;
 import com.capgemini.dnd.customexceptions.DisplayException;
 import com.capgemini.dnd.customexceptions.RMOrderNotAddedException;
