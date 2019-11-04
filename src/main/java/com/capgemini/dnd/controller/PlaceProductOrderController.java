fc870959ed5525db6935a21b0d58834dda546b07
akhi-molu <55138267+akhi-molu@users.noreply.github.com>
2019-11-04 12:21:37 +0530

    minor changes

--- src/main/java/com/capgemini/dnd/controller/PlaceProductOrderController.java
+++ src/main/java/com/capgemini/dnd/controller/PlaceProductOrderController.java
@@ -9,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.dnd.customexceptions.ConnectionException;
@@ -29,7 +30,8 @@ public class PlaceProductOrderController {
 
 	ProductOrder productOrder;
 
-	@GetMapping("/PlaceOrder")
+//	@GetMapping("/PlaceOrder")
+	@RequestMapping(method = RequestMethod.POST)
 	public String trackProductOrder(@RequestParam("name") String name, @RequestParam("supplierId") String supplierId,
 			@RequestParam("quantityValue") Double quantityValue, @RequestParam("quantityUnit") String quantityUnit,
 			@RequestParam("dateOfDelivery") @DateTimeFormat(pattern = "yyyy-MM-dd") String dateOfDelivery,
