af76a6289adaa9fae09462462434003d261e3a1f
gaikwadgaurav997 <37701250+gaikwadgaurav997@users.noreply.github.com>
2019-11-05 17:07:49 +0530

    error resolved

--- src/main/java/com/capgemini/dnd/dao/RawMaterialDAOImpl.java
+++ src/main/java/com/capgemini/dnd/dao/RawMaterialDAOImpl.java
@@ -555,9 +555,7 @@ public class RawMaterialDAOImpl implements RawMaterialDAO {
 			session = sessionFactory.openSession();
 			session.beginTransaction();
 
-			try {
-				RawMaterialStockEntity rmStockEntity = session.load(RawMaterialStockEntity.class,
-						Integer.parseInt(rawMaterialStock.getOrderId()));
+	
 
 
 	        try {
