aeb8756c795a81b5245066e23f8c359abb4aec5b
dgupta23 <55138198+dgupta23@users.noreply.github.com>
2019-11-05 14:39:07 +0530

    junit stock case done

--- src/test/java/com/capgemini/dnd/dao/RawMaterialDAOImplTestSpring.java
+++ src/test/java/com/capgemini/dnd/dao/RawMaterialDAOImplTestSpring.java
@@ -1,6 +1,10 @@
 package com.capgemini.dnd.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.capgemini.dnd.customexceptions.IncompleteDataException;
+import com.capgemini.dnd.customexceptions.ProcessDateException;
 import com.capgemini.dnd.customexceptions.RMOrderIDDoesNotExistException;
 import com.capgemini.dnd.dto.RawMaterialStock;
 
@@ -114,7 +121,7 @@ public class RawMaterialDAOImplTestSpring {
 	@Rollback(true)
 	public void testTrackRawMaterialOrder1() {
 
-		RawMaterialStock rawMaterialStock = new RawMaterialStock("5");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("2");
 		String actualMessage = null;
 		try {
 			if(rawMaterialDAO.doesRawMaterialOrderIdExist(rawMaterialStock.getOrderId())) {
@@ -123,7 +130,7 @@ public class RawMaterialDAOImplTestSpring {
 		} catch (RMOrderIDDoesNotExistException e) {
 			actualMessage = e.getMessage();
 		}
-		String expectedMessage = "The order ID had been in the warehouse with warehouseID = w01 from 2019-08-14 05:30:00.0 to 2019-11-01 05:30:00.0(79 days)";
+		String expectedMessage = "The order ID had been in the warehouse with warehouseID = w01 from 2019-08-03 05:30:00.0 to 2019-10-07 05:30:00.0(65 days)";
 		assertEquals(expectedMessage, actualMessage);
 		
 	}
@@ -147,30 +154,138 @@ public class RawMaterialDAOImplTestSpring {
 		
 	}
 
-//	@Test
-//	void testDoesRawMaterialOrderIdExist() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	void testProcessDateCheck() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	void testUpdateProcessDateinStock() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	void testUpdateRawMaterialStock() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	void testDoesRawMaterialOrderIdExistInStock() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testDoesRawMaterialOrderIdExist1() throws RMOrderIDDoesNotExistException {
+		assertTrue(rawMaterialDAO.doesRawMaterialOrderIdExist("5"));
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testDoesRawMaterialOrderIdExist2() throws RMOrderIDDoesNotExistException {
+		
+		assertThrows(RMOrderIDDoesNotExistException.class, () -> {
+			rawMaterialDAO.doesRawMaterialOrderIdExist("500");
+			});
+	}
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testProcessDateCheck1() throws ProcessDateException, IncompleteDataException, ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("1",sdf.parse("2019-10-09"));
+		
+		
+			 boolean actualMessage = rawMaterialDAO.processDateCheck(rawMaterialStock);
+			 assertTrue(actualMessage);
+			
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testProcessDateCheck2() throws ProcessDateException, IncompleteDataException, ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("1",sdf.parse("2020-10-09"));
+		assertThrows(ProcessDateException.class, () -> {
+				 rawMaterialDAO.processDateCheck(rawMaterialStock);
+				});
+			
+	}
+
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testUpdateProcessDateinStock1() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("5", sdf.parse("2019-10-09"));
+		String actualMessage = null;
+		try {
+			if(rawMaterialDAO.doesRawMaterialOrderIdExist(rawMaterialStock.getOrderId())) {
+			actualMessage = rawMaterialDAO.updateProcessDateinStock(rawMaterialStock);
+			}
+		} catch (RMOrderIDDoesNotExistException e) {
+			actualMessage = e.getMessage();
+		}
+		String expectedMessage = "Data inserted";
+		assertEquals(expectedMessage, actualMessage);
+	}
+
+		
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testUpdateProcessDateinStock2() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("500", sdf.parse("2019-10-09"));
+		String actualMessage = null;
+		try {
+			if(rawMaterialDAO.doesRawMaterialOrderIdExist(rawMaterialStock.getOrderId())) {
+			actualMessage = rawMaterialDAO.trackRawMaterialOrder(rawMaterialStock);
+			}
+		} catch (RMOrderIDDoesNotExistException e) {
+			actualMessage = e.getMessage();
+		}
+		String expectedMessage = "RawMaterial Order ID does not exist";
+		assertEquals(expectedMessage, actualMessage);
+	}
+	
+	
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testUpdateRawMaterialStock1() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("100", sdf.parse("2019-10-09"), sdf.parse("2019-10-09"), "Passed");
+		String actualMessage = null;
+		try {
+			if(rawMaterialDAO.doesRawMaterialOrderIdExist(rawMaterialStock.getOrderId())) {
+			actualMessage = rawMaterialDAO.updateRawMaterialStock(rawMaterialStock);
+			}
+		} catch (RMOrderIDDoesNotExistException e) {
+			actualMessage = e.getMessage();
+		}
+		String expectedMessage = "RawMaterial Order ID does not exist";
+		assertEquals(expectedMessage, actualMessage);
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testUpdateRawMaterialStock2() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		RawMaterialStock rawMaterialStock = new RawMaterialStock("5", sdf.parse("2019-09-02"), sdf.parse("2020-02-02"), "Passed");
+		String actualMessage = null;
+		try {
+			if(rawMaterialDAO.doesRawMaterialOrderIdExist(rawMaterialStock.getOrderId())) {
+			actualMessage = rawMaterialDAO.updateRawMaterialStock(rawMaterialStock);
+			}
+		} catch (RMOrderIDDoesNotExistException e) {
+			actualMessage = e.getMessage();
+		}
+		String expectedMessage = "Data inserted";
+		assertEquals(expectedMessage, actualMessage);
+	}
+	
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testDoesRawMaterialOrderIdExistInStock1() {
+		assertTrue(rawMaterialDAO.doesRawMaterialOrderIdExistInStock("5"));
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testDoesRawMaterialOrderIdExistInStock2() {
+		assertFalse(rawMaterialDAO.doesRawMaterialOrderIdExistInStock("500"));
+	}
 
 	
 
